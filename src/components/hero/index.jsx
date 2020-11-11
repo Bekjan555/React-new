f1f4a44e3cc0277631e4dc82c9df62afbe4c212a
Bekjan555 <mr.bayganaliev@mail.ru>
2020-11-11 16:11:49 +0600

    new proect

---
+++ src/components/hero/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './styles.css';
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <div className="container">
+                <div className="hero__content">
+                    <div className="hero__product">
+                        <h1 className="hero__title">
+                        Tresemme Conditioner    
+                        </h1>
+                        <h3 className="hero__suptitle">
+                            Naturals 
+                        </h3>
+                        <p className="hero__description">
+Nourishing Moisture 740 ml
+                        </p>
+                        <span className="hero__prise">
+
+                        </span>
+                        <a href="#" className="hero__btn">
+
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
